452d7502e3c2fb179889ca8e0dceba4a5d56404d
cpgeier <cpg3rb@virginia.edu>
2019-12-03 19:13:37 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const app = require('express')();
+const http = require('http').Server(app);
+
+const port = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send("WWO!");
+});
+
+http.listen(port, () => {
+    console.log(`Listening on *:${port}`);
+});
